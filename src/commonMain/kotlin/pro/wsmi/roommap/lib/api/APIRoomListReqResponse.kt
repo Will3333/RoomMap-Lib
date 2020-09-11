a44e5bc3f4cc3cd5f094bef0cb7adeaa68f77302
William Smith <william.smith@wsmi.pro>
2020-09-12 01:53:50 +0200

    Correction of rooms attribute type of APIRoomListReqResponse.kt

--- src/commonMain/kotlin/pro/wsmi/roommap/lib/api/APIRoomListReqResponse.kt
+++ src/commonMain/kotlin/pro/wsmi/roommap/lib/api/APIRoomListReqResponse.kt
@@ -6,4 +6,4 @@ import kotlinx.serialization.Serializable
 
 @ExperimentalSerializationApi
 @Serializable
-data class APIRoomListReqResponse (@SerialName("matrix_rooms") val matrixRooms: Map<MatrixServer, MatrixRoom>) : APIResponse
+data class APIRoomListReqResponse (@SerialName("matrix_rooms") val matrixRooms: Map<MatrixServer, List<MatrixRoom>>) : APIResponse
